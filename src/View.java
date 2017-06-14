b40913810f0ea74b33e54b6bb155ec7508d8d33c
Kiki <kiki@Kikis-MBP.fritz.box>
2017-06-14 19:27:44 +0200

    Kleinere Codeteile
    Hinzufügen von kleineren Codeteilen im View sowie in der Startklasse
    SchiffeVersenken

--- src/View.java
+++ src/View.java
@@ -1,4 +1,23 @@
+import java.awt.BorderLayout;
 
-public class View {
+import javax.swing.JFrame;
 
+public class View extends JFrame {
+
+	public View() {
+		super("Schiffe versenken");
+		fensterGenerieren();
+	}
+
+	/*
+	 * Methode welche das Fenster in dem das Spiel zu sehen und spielen ist erstellt wird
+	 */
+	private void fensterGenerieren() {
+		setSize(800, 500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setLayout(new BorderLayout());
+		setVisible(true);
+		
+	}
 }
