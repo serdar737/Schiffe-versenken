0430f6db60f500967ac780bc118e6915380bd4e8
Serdar <serdarsarsar@gmx.de>
2017-06-20 11:31:26 +0200

    Fenstergröße angepasst

--- src/View.java
+++ src/View.java
@@ -18,7 +18,7 @@ public class View extends JFrame {
 	 * Methode, welche das Fenster in dem das Spiel zu sehen und spielen ist erstellt wird
 	 */
 	private void fensterGenerieren() {
-		setSize(1200, 700);
+		setSize(1100, 700);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setLayout(new BorderLayout());
