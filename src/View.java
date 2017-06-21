a30c7848c23af23111c2c70b58d8d6191959f728
Serdar <serdarsarsar@gmx.de>
2017-06-21 13:56:22 +0200

    Ein paar versuche, um zwei Spielfelder nebeneinander zu bauen. -zweites Panel hinzugefügt -setResizable = true -JTextfield hinzugefügt -Die Variable feldgroesse hinzugefügt, um später die Spielfeldgroeße anhand des Schwierigkeitsgrades zu wählen

--- src/View.java
+++ src/View.java
@@ -4,10 +4,14 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class View extends JFrame {
-	private JButton[][] button = new JButton [10][10];
-	private GamePanel panel;
+	int feldgroesse = 10;
+	private JButton[][] button = new JButton [feldgroesse][feldgroesse];
+	private JTextField text;
+	private GamePanel panel1, panel2;
+
 
 	public View() {
 		super("Schiffe versenken");
@@ -20,24 +24,27 @@ public class View extends JFrame {
 	private void fensterGenerieren() {
 		setSize(1100, 700);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(false);
+		setResizable(true);
 		setLayout(new BorderLayout());
-		setVisible(true);
 		//Panel
-		panel = new GamePanel();
-		add(panel, BorderLayout.EAST);
+		text = new JTextField("Platzhalter");
+		add(text, BorderLayout.PAGE_START);
+		panel1 = new GamePanel(feldgroesse);
+		add(panel1, BorderLayout.LINE_START);
+		panel2 = new GamePanel(feldgroesse);
+		add(panel2, BorderLayout.LINE_END);
 		setVisible(true);
 		
 	}
 	
 	class GamePanel extends JPanel {
-		
 
 		
-		public GamePanel() {
-			setLayout(new GridLayout(10, 10));
-			for (int k=0; k<10; k++) {
-				for (int h=0; h<10; h++) {
+		public GamePanel(int i) {
+			setLayout(new GridLayout(10,10));
+			setSize(100, 100);
+			for (int k=0; k<i; k++) {
+				for (int h=0; h<i; h++) {
 					button[k][h] = new JButton();
 					add(button[k][h]);
 					button[k][h].setBorderPainted(true);
