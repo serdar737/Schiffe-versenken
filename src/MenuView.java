08b658a8bbf25942b2299252f4d29defbfd4fa67
Kiki <kiki@Kikis-MBP.fritz.box>
2017-07-09 16:16:05 +0200

    View Klassen hinzugefügt
    Eine View Klasse für das Hauptmenü sowie eine für das setzten der
    Schiffe hinzugefügt.
    Einfach damit wird schonmal haben.

---
+++ src/MenuView.java
@@ -0,0 +1,10 @@
+/**
+ * Layout für das Hauptmenue-Fenster des Spiels.
+ * Beinhaltet Buttons um ein Spiel zu starten, einen um die Regeln zu lesen und einen um
+ * das Spiel zu beenden.
+ * @author kiki
+ *
+ */
+public class MenuView {
+
+}
---
+++ src/SetShipView.java
@@ -0,0 +1,8 @@
+/**
+ * Layout für das Fenster in dem der Spieler seine Schiffe auf das Feld setzt
+ * @author kiki
+ *
+ */
+public class SetShipView {
+
+}
