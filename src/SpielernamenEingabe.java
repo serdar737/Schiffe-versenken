2e84a4fb5cdfbcbe0f438991d10b5261986cd80f
Kiki <kiki@Kikis-MBP.fritz.box>
2017-09-19 19:09:13 +0200

    Spielereingabe überarbeitet
    View im Grunde vollständig, auch hier noch nen bisschen Logik einbauen.

--- src/SpielernamenEingabe.java
+++ src/SpielernamenEingabe.java
@@ -5,7 +5,9 @@ import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -13,7 +15,10 @@ import javax.swing.border.EmptyBorder;
  */
 public class SpielernamenEingabe extends JFrame {
 	
-	private ButtonPanel bpanel ;
+	private ButtonPanel bpanel;
+	private JTextField field1, field2;
+	private NamePanel npanel;
+	private JLabel label;
 
 	public SpielernamenEingabe(){
 		super("Spielernamen eingeben");
@@ -29,6 +34,9 @@ public class SpielernamenEingabe extends JFrame {
 		bpanel = new ButtonPanel();
 		add(bpanel, BorderLayout.SOUTH);
 		bpanel.setBorder(new EmptyBorder(20,20,20,20));
+		npanel = new NamePanel();
+		add(npanel, BorderLayout.CENTER);
+		npanel.setBorder(new EmptyBorder(20,20,20,20));
 		setVisible(true);
 	}
 	
@@ -50,6 +58,33 @@ public class SpielernamenEingabe extends JFrame {
 				button[0][0].setBorderPainted(true);
 				button[0][0].setPreferredSize(new Dimension(120,30));
 				button[0][0].setText("Bestätigen");
+		}
+	}
+	
+	class NamePanel extends JPanel{
+		
+		public NamePanel(){
+			
+			GridBagLayout gblayout = new GridBagLayout();
+			setLayout(gblayout);
+			GridBagConstraints constraints = new GridBagConstraints();
+			
+			constraints.gridy = 0;
+			constraints.gridx = 0;
+			
+			label = new JLabel();
+			label.setText("Bitte die Spielernamen eingeben (optional):");
+			add(label, constraints);
+			
+			constraints.gridy++;
+			field1 = new JTextField("Spielername 1", 10);
+			add(field1, constraints);
+			field1.setSize(200,70);
+			constraints.gridy++;
+			
+			field2 = new JTextField("Spielername 2", 10);
+			add(field2, constraints);
+			field2.setSize(200,70);
 		}
 	}
 }
