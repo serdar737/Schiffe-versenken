c07ef9e777de4056b4cd20e8fd967af36b1604d9
Kiki <kiki@Kikis-MBP.fritz.box>
2017-09-24 18:45:33 +0200

    Zeugs

--- src/ManualView.java
+++ src/ManualView.java
@@ -4,12 +4,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
--- src/OptionsView.java
+++ src/OptionsView.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 public class OptionsView extends JFrame {
